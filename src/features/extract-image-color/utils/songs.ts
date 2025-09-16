6a173ddc6a20c2e5347299ed83db3b3a78719b2e
AskatAsh <abdulaskat@gmail.com>
2025-09-16 23:34:04 +0600

    added one more song data and change src of one of them

--- src/features/extract-image-color/utils/songs.ts
+++ src/features/extract-image-color/utils/songs.ts
@@ -11,7 +11,7 @@ export const songs: TSong[] = [
         id: 2,
         title: "Levitating",
         artist: "Dua Lipa",
-        image: "https://images.unsplash.com/photo-1586095087956-bc66fe634955?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c29uZ3xlbnwwfHwwfHx8MA%3D%3D"
+        image: "https://images.unsplash.com/photo-1606614520047-8ad516d9d84b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c29uZ3N8ZW58MHx8MHx8fDA%3D"
     },
     {
         id: 3,
@@ -30,5 +30,11 @@ export const songs: TSong[] = [
         title: "Peaches",
         artist: "Justin Bieber",
         image: "https://images.unsplash.com/photo-1510915361894-db8b60106cb1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c29uZ3xlbnwwfHwwfHx8MA%3D%3D"
+    },
+    {
+        id: 6,
+        title: "Beautiful Life",
+        artist: "Jungkook",
+        image: "https://images.unsplash.com/photo-1553236152-72dd67ab92ed?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHNvbmdzfGVufDB8fDB8fHww"
     }
 ];
